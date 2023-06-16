dbbdae1c4706ca473c4bfcf116e6b1aaa163ddcc
hin6150 <hin6150@gmail.com>
2023-06-16 09:49:04 +0900

    리엑트 네이티브 다이어리 앱 생성

---
+++ DayLog/App.jsx
@@ -0,0 +1,8 @@
+import {NavigationContainer} from '@react-navigation/native';
+import React from 'react';
+
+const App = () => {
+  return <NavigationContainer></NavigationContainer>;
+};
+
+export default App;
