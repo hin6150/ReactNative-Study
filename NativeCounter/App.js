db1e46d6f970f5716f7fabd52dc36e6fd5ad144f
hin6150 <hin6150@gmail.com>
2023-07-07 22:45:49 +0900

    Feat - 리액트 네이티브 UI 컴포넌트 개발 완료

--- NativeCounter/App.js
+++ NativeCounter/App.js
@@ -1,8 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Counter from './Counter';
 
 const App = () => {
-  return <Counter style={{flex: 1}} />;
+  const [value, setValue] = useState(0);
+  const onPressLeftButton = () => {
+    setValue(value + 1);
+  };
+  const onPressRightButton = () => {
+    setValue(value - 1);
+  };
+  return (
+    <Counter
+      style={{flex: 1}}
+      value={value}
+      leftButtonText="+1"
+      rightButtonText="-1"
+      onPressLeftButton={onPressLeftButton}
+      onPressRightButton={onPressRightButton}
+    />
+  );
 };
 
 export default App;
