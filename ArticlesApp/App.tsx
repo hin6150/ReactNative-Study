eb8b51cfc4e92ca20850cbf1b08a692c594b1080
hin6150 <hin6150@gmail.com>
2023-07-19 22:33:50 +0900

    Feat - 게시판 앱 만들기

---
+++ ArticlesApp/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {SafeAreaView, Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>안녕 리액트 네이티브</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default App;
